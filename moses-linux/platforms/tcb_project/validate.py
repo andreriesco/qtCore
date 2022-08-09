6cd1119e758d62ec08faae226dc02144901b8ec0
andreriesco <83607022+andreriesco@users.noreply.github.com>
2022-08-09 14:59:51 -0300

    Delete moses-linux directory

--- moses-linux/platforms/tcb_project/validate.py
+++
@@ -1,9 +0,0 @@
-"""Platform-specific validation."""
-import validation
-import validation_utils
-
-def init_app_tables(
-    application: validation.ExtendedValidation) -> None:
-    """Add platform-specific validation to application object."""
-    # nothing to add
-
